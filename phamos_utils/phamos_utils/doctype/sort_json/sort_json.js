3f634b29e44004b101eea9cfaddd51d258eda9c7
Roque Vera <roque.vera@phamos.eu>
2025-04-04 15:47:27 -0300

    feat: Implement sorting functionality for JSON data in SortJSON DocType

--- phamos_utils/phamos_utils/doctype/sort_json/sort_json.js
+++ phamos_utils/phamos_utils/doctype/sort_json/sort_json.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, Phamos GmbH and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sort JSON", {
-// 	refresh(frm) {
+frappe.ui.form.on("Sort JSON", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+
+    sort_json: function(frm) {
+        frappe.call({
+            method: "phamos_utils.phamos_utils.doctype.sort_json.sort_json.sort_json",
+            args: {
+                json_data: frm.doc.input_json
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frm.set_value("sorted", r.message);
+                }
+            }
+        });
+    }
+});
--- phamos_utils/phamos_utils/doctype/sort_json/sort_json.py
+++ phamos_utils/phamos_utils/doctype/sort_json/sort_json.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2025, Phamos GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SortJSON(Document):
 	pass
+
+@frappe.whitelist()
+def sort_json(json_data):
+	"""
+	Sorts the given JSON data by 'dt' (Doctype) and 'name' if applicable.
+	"""
+	if isinstance(json_data, str):
+		json_data = frappe.parse_json(json_data)
+		
+	if isinstance(json_data, list):
+		json_data.sort(key=lambda x: (x.get("name", "")))
+
+	json_data = frappe.as_json(json_data, indent=4, ensure_ascii=False)
+	return json_data
